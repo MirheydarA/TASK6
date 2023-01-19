a2904eb8fa4418f2668fc49ca346c81094e002fd
MirheydarA <a.mirheyder004@gmail.com>
2023-01-19 17:08:26 +0400

    task6

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿initial: int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+string sign = Console.ReadLine();
+switch (sign)
+{
+	case "+":
+		Console.WriteLine(number1 + number2);
+		break;
+	case "-":
+		Console.WriteLine(number1 - number2);
+		break;
+		
+	case "*":
+			Console.WriteLine(number1 * number2);
+		        break;
+		case "/":
+			Console.WriteLine(number1 / number2);
+		        break;
+
+
+
+
+
+	default: 
+Console.Clear();
+		Console.WriteLine("wrong input");
+		break;
+}
+goto initial;
